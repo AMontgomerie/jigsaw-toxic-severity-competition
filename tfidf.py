69a333821b6ec193b652d6a90187d20820537313
Adam Montgomerie <adam@avanssion.com>
2021-12-02 22:28:38 +0900

    add tfidf script

--- tfidf.py
+++ tfidf.py
@@ -11,7 +11,9 @@ from typing import List
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--train_data_path", type=str, default="data/train.csv")
-    parser.add_argument("--test_data_path", type=str, default="data/valid.csv")
+    parser.add_argument("--valid_path", type=str, default="data/valid.csv")
+    parser.add_argument("--test_path", type=str, default="data/comments_to_score.csv")
+    parser.add_argument("--save_path", type=str, default="./submission.csv")
     return parser.parse_args()
 
 
@@ -32,20 +34,38 @@ def test(models: List[Pipeline], test_data: pd.DataFrame) -> float:
     return sum(mean_less_toxic < mean_more_toxic) / len(test_data)
 
 
+def predict(
+    models: List[Pipeline], test_data: pd.DataFrame, submission_path: str
+) -> None:
+    print("Generating predictions...")
+    fold_predictions = []
+    for model in models:
+        predictions = model.predict(test_data.text)
+        fold_predictions.append(predictions)
+    mean_predictions = np.mean(fold_predictions, axis=0)
+    submission = pd.DataFrame(
+        {"comment_id": test_data.comment_id, "score": mean_predictions}
+    )
+    submission.to_csv(submission_path, index=False)
+    print(f"Saved predictions to {submission_path}")
+
+
 if __name__ == "__main__":
     args = parse_args()
     data = pd.read_csv(args.train_data_path)
+    valid_data = pd.read_csv(args.test_data_path)
     test_data = pd.read_csv(args.test_data_path)
     models = []
-    valid_scores = []
+    oof_scores = []
     for fold in range(5):
         train_data = data[data.fold != fold]
-        valid_data = data[data.fold == fold]
+        oof_data = data[data.fold == fold]
         model = train(train_data)
-        preds = model.predict(valid_data.text)
-        mse = mean_squared_error(valid_data.target, preds)
+        predictions = model.predict(oof_data.text)
+        mse = mean_squared_error(oof_data.target, predictions)
         models.append(model)
-        valid_scores.append(mse)
-    print(f"validation MSE by fold: {valid_scores}")
-    test_score = test(models, test_data)
-    print(test_score)
+        oof_scores.append(mse)
+    print(f"validation MSE by fold: {oof_scores}")
+    test_score = test(models, valid_data)
+    print(f"ranking test score (mean of 5 folds): {test_score}")
+    predict(models, test_data, args.save_path)
