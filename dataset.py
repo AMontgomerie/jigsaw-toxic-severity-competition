3e75e866956db5ab55b78ea1d3650aa6987a0ffd
Adam Montgomerie <adam@avanssion.com>
2021-12-03 22:07:55 +0900

    make labels optional

--- dataset.py
+++ dataset.py
@@ -6,24 +6,35 @@ from typing import Mapping
 
 
 class ToxicDataset(Dataset):
-    def __init__(self, data: pd.DataFrame, tokenizer: AutoTokenizer) -> None:
-        self.data = data
+    def __init__(
+        self,
+        texts: pd.Series,
+        tokenizer: AutoTokenizer,
+        max_length: int,
+        labels: pd.Series = None,
+    ) -> None:
+        self.texts = texts
+        self.labels = labels
         self.tokenizer = tokenizer
+        self.max_length = max_length
 
     def __len__(self) -> int:
         return len(self.data)
 
     def __getitem__(self, index: int) -> Mapping[torch.Tensor, torch.Tensor]:
-        item = self.data.loc[index]
+        item = self.texts.loc[index]
         encoded_item = self.tokenizer(
             item.text,
-            max_length=512,
+            max_length=self.max_length,
             padding="max_length",
             truncation=True,
             return_tensors="pt",
         )
-        return {
+        inputs = {
             "input_ids": encoded_item["input_ids"].squeeze(),
             "attention_mask": encoded_item["attention_mask"].squeeze(),
-            "labels": torch.tensor(item.target, dtype=torch.float32),
         }
+        if self.labels:
+            label = self.labels.loc[index]
+            inputs["labels"] = torch.tensor(label, dtype=torch.float32)
+        return inputs
