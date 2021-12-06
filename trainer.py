3567ca591fd1eac969b8d74ab3f702fe30ef8c08
Adam Montgomerie <adam@avanssion.com>
2021-12-06 14:38:05 +0900

    remove hardcoded log_freq param

--- trainer.py
+++ trainer.py
@@ -188,9 +188,9 @@ class PairedTrainer(Trainer):
         self.wandb_train_loss = AverageMeter()
 
     def train(self) -> None:
-        wandb.watch(self.model, self.loss_fn, log="all", log_freq=10)
+        wandb.watch(self.model, self.loss_fn, log="all", log_freq=self.log_interval)
         wandb.log({"valid_score": 0})
-        global_step = 0
+        global_step = 1
         for epoch in range(1, self.epochs + 1):
             self.model.train()
             self.train_loss.reset()
