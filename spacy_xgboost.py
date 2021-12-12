44cdff9d7124b328eddb9e4083692b64738bfb2f
Adam Montgomerie <adam@avanssion.com>
2021-12-12 14:38:34 +0900

    add spacy_xgboost

---
+++ spacy_xgboost.py
@@ -0,0 +1,73 @@
+import argparse
+import pandas as pd
+import numpy as np
+import spacy
+import joblib
+import os
+from sklearn.base import BaseEstimator
+from xgboost import XGBRegressor
+from typing import List
+
+
+class SpacyVectorizer:
+    def __init__(self) -> None:
+        self.tokenizer = spacy.load("en_core_web_lg")
+
+    def transform(self, texts: pd.Series) -> List[np.ndarray]:
+        print(f"Encoding {len(texts)} texts...")
+        tokenized_texts = list(self.tokenizer.pipe(texts))
+        text_vectors = []
+        for text in tokenized_texts:
+            vectors = [token.vector for token in text]
+            mean_vector = np.mean(vectors, axis=0)
+            text_vectors.append(mean_vector)
+        return text_vectors
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train_path", type=str, default="data/train.csv")
+    parser.add_argument("--valid_path", type=str, default="data/valid.csv")
+    parser.add_argument("--test_path", type=str, default="data/comments_to_score.csv")
+    parser.add_argument("--model_save_dir", type=str, default=".")
+    parser.add_argument("--pred_save_path", type=str, default="./submission.csv")
+    return parser.parse_args()
+
+
+def get_valid_score(
+    model: BaseEstimator, less_toxic: List[np.ndarray], more_toxic: List[np.ndarray]
+) -> float:
+    less_toxic_preds = model.predict(less_toxic)
+    more_toxic_preds = model.predict(more_toxic)
+    return sum(less_toxic_preds < more_toxic_preds) / len(test_data)
+
+
+def save_model(model: BaseEstimator, fold: int, save_dir: str) -> None:
+    save_path = os.path.join(save_dir, f"tfidf_ridge_fold_{fold}.pkl")
+    joblib.dump(model, save_path)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    data = pd.read_csv(args.train_path)
+    valid_data = pd.read_csv(args.valid_path)
+    test_data = pd.read_csv(args.test_path)
+    encoder = SpacyVectorizer()
+    data["vector"] = encoded_texts = encoder.transform(data.text)
+    encoded_less_toxic = encoder.transform(valid_data.less_toxic)
+    encoded_more_toxic = encoder.transform(valid_data.more_toxic)
+    encoded_test_data = encoder.transform(test_data.text)
+    fold_predictions = []
+    for fold in range(5):
+        train = data.loc[data.fold == fold]
+        model = XGBRegressor()
+        model.fit(train.vector.tolist(), train.target)
+        valid_score = get_valid_score(model, encoded_less_toxic, encoded_more_toxic)
+        print(f"Fold {fold} ranking score: {valid_score}")
+        predictions = model.predict(encoded_test_data)
+        fold_predictions.append(predictions)
+    mean_predictions = np.mean(fold_predictions, axis=0)
+    submission = pd.DataFrame(
+        {"comment_id": test_data.comment_id, "score": mean_predictions}
+    )
+    submission.to_csv(args.pred_save_path, index=False)
