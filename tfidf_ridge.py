b9225950c7f6d2ab8971c2d3dde7de9ebc1264de
Adam Montgomerie <adam@avanssion.com>
2021-12-24 12:43:26 +0900

    add support for single fold tfidfridge models

--- tfidf_ridge.py
+++ tfidf_ridge.py
@@ -19,38 +19,60 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--model_save_dir", type=str, default=".")
     parser.add_argument("--pred_save_path", type=str, default="./submission.csv")
     parser.add_argument("--ridge_alpha", type=float, default=None)
+    parser.add_argument("--max_alpha", type=float, default=1.0)
     parser.add_argument("--tokenization_scheme", type=str, default="word")
     parser.add_argument("--min_df", type=int, default=0)
     parser.add_argument("--max_df", type=float, default=0.8),
     parser.add_argument("--ngram_min", type=int, default=1),
     parser.add_argument("--ngram_max", type=int, default=2)
     parser.add_argument("--seed", type=int, default=666)
+    parser.add_argument("--num_folds", type=int, default=5)
     return parser.parse_args()
 
 
 def train(
+    train_data: pd.DataFrame,
+    valid_data: pd.DataFrame,
+    encoder: TfidfVectorizer,
+    alpha: float = None,
+    max_alpha: float = 1.0,
+):
+    best_model = None
+    if alpha is None:
+        best_alpha = None
+        best_score = 0
+        for alpha in np.linspace(0.1, max_alpha, max_alpha * 10):
+            regressor = Ridge(alpha=alpha)
+            model = Pipeline([("tfidf", encoder), ("ridge", regressor)])
+            model.fit(train_data.text, train_data.target)
+            score = validate([model], valid_data)
+            if score > best_score:
+                best_score = score
+                best_alpha = alpha
+                best_model = model
+            print(f"alpha: {alpha} | score: {score}")
+    else:
+        regressor = Ridge(alpha=alpha)
+        best_model = Pipeline([("tfidf", encoder), ("ridge", regressor)])
+        best_model.fit(train_data.text, train_data.target)
+        best_score = validate([best_model], valid_data)
+    print(f"final alpha: {best_alpha} | final score: {best_score}")
+
+
+def train_fold(
     fold: int,
     train_data: pd.DataFrame,
     oof_data: pd.DataFrame,
-    tokenization_scheme: str,
-    min_df: int,
-    max_df: float,
-    ngram_min: int,
-    ngram_max: int,
+    encoder: TfidfVectorizer,
     alpha: float = None,
+    max_alpha: float = 1.0,
 ) -> Pipeline:
-    encoder = TfidfVectorizer(
-        analyzer=tokenization_scheme,
-        min_df=min_df,
-        max_df=max_df,
-        ngram_range=(ngram_min, ngram_max),
-    )
     min_mse = float("inf")
     best_model = None
     best_alpha = alpha
     if alpha is None:  # if no alpha is supplied then tune it
         print(f"Tuning alpha for fold {fold}...")
-        for alpha in np.linspace(0.1, 1, 20):
+        for alpha in np.linspace(0.1, max_alpha, max_alpha * 10):
             regressor = Ridge(alpha=alpha)
             model = Pipeline([("tfidf", encoder), ("ridge", regressor)])
             model.fit(train_data.text, train_data.target)
@@ -72,17 +94,32 @@ def train(
     return best_model, min_mse
 
 
-def make_folds(data: pd.DataFrame, seed: int) -> pd.DataFrame:
+def get_encoder(
+    tokenization_scheme: str,
+    min_df: int,
+    max_df: float,
+    ngram_min: int,
+    ngram_max: int,
+) -> TfidfVectorizer:
+    encoder = TfidfVectorizer(
+        analyzer=tokenization_scheme,
+        min_df=min_df,
+        max_df=max_df,
+        ngram_range=(ngram_min, ngram_max),
+    )
+    return encoder
+
+
+def make_folds(data: pd.DataFrame, num_folds: int, seed: int) -> pd.DataFrame:
     skf = StratifiedKFold(shuffle=True, random_state=seed)
-    bins = 5
-    stratified_targets = pd.cut(data.target, bins, labels=False)
+    stratified_targets = pd.cut(data.target, num_folds, labels=False)
     data["fold"] = -1
-    for fold, _, valid_index in enumerate(skf.split(data.text, stratified_targets)):
-        data.loc[valid_index] = fold
+    for fold, (_, valid_index) in enumerate(skf.split(data.text, stratified_targets)):
+        data.loc[valid_index, "fold"] = fold
     return data
 
 
-def test(models: List[Pipeline], test_data: pd.DataFrame) -> float:
+def validate(models: List[Pipeline], test_data: pd.DataFrame) -> float:
     less_toxic_scores = []
     more_toxic_scores = []
     for model in models:
@@ -114,28 +151,45 @@ if __name__ == "__main__":
     test_data = pd.read_csv(args.test_path)
     models = []
     mse_scores = []
-    if "fold" not in data.columns:
-        data = make_folds(data, args.seed)
-    for fold in range(5):
-        train_data = data[data.fold != fold]
-        oof_data = data[data.fold == fold]
-        model, mse = train(
-            fold,
-            train_data,
-            oof_data,
-            args.tokenization_scheme,
-            args.min_df,
-            args.max_df,
-            args.ngram_min,
-            args.ngram_max,
-            args.ridge_alpha,
-        )
-        mse_scores.append(mse)
+    encoder = get_encoder(
+        args.tokenization_scheme,
+        args.min_df,
+        args.max_df,
+        args.ngram_min,
+        args.ngram_max,
+    )
+    if args.num_folds > 1:
+        if "fold" not in data.columns:
+            data = make_folds(data, args.num_folds, args.seed)
+        for fold in range(args.num_folds):
+            train_data = data[data.fold != fold]
+            oof_data = data[data.fold == fold]
+            model, mse = train_fold(
+                fold,
+                train_data,
+                oof_data,
+                args.tokenization_scheme,
+                args.min_df,
+                args.max_df,
+                args.ngram_min,
+                args.ngram_max,
+                args.ridge_alpha,
+                args.max_alpha,
+            )
+            mse_scores.append(mse)
+            models.append(model)
+            save_model(model, fold, args.model_save_dir)
+        print(f"cv mse: {np.mean(mse_scores)}")
+        valid_score = validate(models, valid_data)
+        print(f"valid score (mean of {args.num_folds} folds): {valid_score}")
+    else:
+        alpha = args.ridge_alpha
+        if alpha is None:
+            print("Defaulting to alpha=1")
+            alpha = 1
+        model = train(data, valid_data, encoder, alpha, args.max_alpha)
         models.append(model)
-        save_model(model, fold, args.model_save_dir)
-    print(f"cv mse: {np.mean(mse_scores)}")
-    test_score = test(models, valid_data)
-    print(f"ranking test score (mean of 5 folds): {test_score}")
+    print("Generating test set predictions...")
     predictions = predict(models, test_data)
     submission = pd.DataFrame(
         {"comment_id": test_data.comment_id, "score": predictions}
